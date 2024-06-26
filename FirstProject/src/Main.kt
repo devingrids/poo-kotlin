80d9143549555646652180e5dde09496732b8c6f
Ingrid Silva <154104419+devingrids@users.noreply.github.com>
2024-06-26 17:57:15 -0300

    Update Main.kt
    introduçao a variaveis

--- FirstProject/src/Main.kt
+++ FirstProject/src/Main.kt
@@ -1,3 +1,42 @@
 fun main() {
-    println("Olá Mundo")
+   var idade = 24
+   val nome = "Milene"
+
+   println(idade)
+   idade = 33
+   println(idade)
+
+   // var - pode ser reatribuida
+   // val - nao pode ser reatribuida
+
+   // var é mutavel e val é imutavel
+
+   // variavel implicita
+   val anoAtual = 2024
+
+   //var explicita
+   val proximoAno: Int = 2025
+
+   //Float
+   val altura = 1.63f
+
+   //Double
+   val PI : Double = 3.14159265359
+
+   //Char
+   val primeiraLetraDoMeuNome: Char = '@'
+
+   //Strings - Fio
+   val name = "Lucas"
+   val sobreNome : String = "Silva"
+
+   //Booleans
+   val voceGostaDeProgramar = true
+
+   val ehMaior = 2 > 3
+   println(ehMaior)
+
+
+
+
 }
